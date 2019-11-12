821b8dd58afbfbb739608490a4389ffb8179c8a0
gql <2551291102@qq.com>
2019-11-12 10:26:05 +0800

    add .gitignore,LICENSE,README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Scala
+<p align="center">
+  Scala 学习笔记
+</p>
+
+## Table of Contents
+
+* [面向对象](#object)
+* [函数式编程](#function)
+
---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: gordon    Email: gordon_ml@163.com
+ * @Date: 11/11/2019
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
